2c8b503125e304ce46b3d9747983b9d9dee5decf
Riverside Rocks <trent@riverside.rocks>
2020-08-14 10:53:39 -0400

    Fix composer.json file

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <?php
 
+namespace commenteer;
 
-    function youtube($username)
-    {
-
-        $file = file_get_contents('comments.json');
-        $comments = json_decode($file, true);
-        $max = count($comments);
-        $total = rand(0, $max);
-        $comment = ($comments[$total]);
-
-        if(isset($username))
+    class youtube{
+        public static function youtube($username)
         {
-            echo "${username}: ${comment}";
-        }
-        else
-        {
-            echo $comment;
+
+            $file = file_get_contents('comments.json');
+            $comments = json_decode($file, true);
+            $max = count($comments);
+            $total = rand(0, $max);
+            $comment = ($comments[$total]);
+
+            if(isset($username))
+            {
+                echo "${username}: ${comment}";
+            }
+            else
+            {
+                echo $comment;
+            }
         }
     }
