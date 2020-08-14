df99a21430dfe9e10f9e55824af203ecf16e2ef3
Riverside Rocks <trent@riverside.rocks>
2020-08-14 09:43:19 -0400

    Start adding comments

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+
+class commenteer{
+
+    public function default($username)
+    {
+
+        $comments = array(json_decode('/comments.json', true));
+
+        if(isset($username))
+        {
+
+        }
+    }
+}
